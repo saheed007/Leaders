11e8c9914938c2b1b57daf6ffe574c69daedbcca
saheed007 <saheedoladejo007@gmail.com>
2022-04-29 18:32:40 +0100

    Add files via upload

---
+++ INSERT INTO presidents (ONE QUERY).sql
@@ -0,0 +1,9 @@
+INSERT INTO presidents (country,continent,president)
+VALUES
+("Egypt"," Africa","Abdel Fattah el-Sisi"),
+("Portugal","Europe","Marcelo Rebelo de Sousa"),
+("Haiti","North America","Jovenel Moise"),
+("Uruguay","South America","Jose Mujica"),
+("Liberia","Africa","Ellen Johnson Sirleaf"),
+("Chile","South America","Michelle Bachelet"),
+("Vietnam","Asia","Tran Dai Quang");
