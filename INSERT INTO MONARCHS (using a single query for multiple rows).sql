26b1790a2bde16350adcf95b44af10e5a2f01c25
saheed007 <saheedoladejo007@gmail.com>
2022-04-29 18:32:25 +0100

    Add files via upload

---
+++ INSERT INTO MONARCHS (using a single query for multiple rows).sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO MONARCHS (country,continent,monarch)
+VALUES ("Brunei", "Asia", "Hassanal Bolkiah"),
+("Oman", "Asia", "Qaboos bin Said al Said"),
+("Norway", "Europe", "Harald V"),
+("Spain", "Europe", "Felipe VI");
